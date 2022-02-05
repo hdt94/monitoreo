b1afbaed58ba78194e7baaaac0f9541df175eec6
hdt94 <vagrant@xenial64>
2022-02-05 17:10:23 +0000

    Update compute deployment

--- infrastructure/terraform/scripts/compute_startup_script.sh
+++ infrastructure/terraform/scripts/compute_startup_script.sh
@@ -6,18 +6,17 @@ if [[ -z $(which docker-compose) ]]; then
     sudo apt-get -qq update
     sudo apt-get -qq install -y ca-certificates gnupg jq lsb-release
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-        | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
+        sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get -qq update
     sudo apt-get -qq install -y docker-ce docker-ce-cli containerd.io
 
     sudo curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
-
 
 APP_DIR=/app/
 
@@ -34,12 +33,23 @@ if [[ $MISSING_APP_DIR = true ]]; then
     git clone ${MONITOREO_REPO} $APP_DIR
 fi
 
+echo "Defining environment..."
 COMPUTE_ENV=$(gcloud secrets versions access ${COMPUTE_SECRET_VERSION} --secret="${COMPUTE_SECRET_ID}")
 
 # Load environment variables
 export $(echo "$COMPUTE_ENV" | sed 's/#.*//g' | xargs)
 
+if [[ -z $GCP_ANALYTICS_ENV_CLOUD_SECRET ]]; then
+    echo "Undefined GCP_ANALYTICS_ENV_CLOUD_SECRET" >2 &
+    exit 1
+fi
+
+export ANALYTICS_ENV="$(gcloud secrets versions access $GCP_ANALYTICS_ENV_CLOUD_SECRET)"
+
 chmod +x $APP_DIR/services/setup-env.prot.sh
 ENV_GCP_FROM_USER=true $APP_DIR/services/setup-env.prot.sh
 
-sudo docker-compose -d --env-file services/.env up --build
+echo "Starting services..."
+docker-compose --env-file $APP_DIR/services/.env build -q
+docker-compose --env-file $APP_DIR/services/.env pull -q
+docker-compose --env-file $APP_DIR/services/.env up -d
--- services/setup-env.prot.sh
+++ services/setup-env.prot.sh
@@ -8,9 +8,24 @@ DIR=$(realpath $(dirname $0))
 ENV_GCP="$(ENV_GCP_FROM_USER=true  $DIR/echo-env-gcp.sh)"
 ENV_SERVICES="$($DIR/echo-env-services.sh)"
 
+if [[ -z $ANALYTICS_ENV ]]; then
+    echo "Undefined ANALYTICS_ENV" >2&
+    exit
+fi
+
+parse_env_analytics_datasource() {
+    echo "
+ANALYTICS_DB_HOST=$(echo "$ANALYTICS_ENV" | grep -oP '(?<='PGHOST=').*')
+ANALYTICS_DB_PASSWORD=$(echo "$ANALYTICS_ENV" | grep -oP '(?<='PGPASSWORD=').*')
+ANALYTICS_DB_PORT=$(echo "$ANALYTICS_ENV" | grep -oP '(?<='PGPORT=').*')
+ANALYTICS_DB_USER=$(echo "$ANALYTICS_ENV" | grep -oP '(?<='PGUSER=').*')
+"
+}
+
 cat << EOF > $DIR/.env
 ${ENV_GCP}
 ${ENV_SERVICES}
+$(parse_env_analytics_datasource)
 GRAFANA_ADMIN_PASSWORD=${GRAFANA_ADMIN_PASSWORD}
 HOST_DOMAIN=${HOST_DOMAIN}
 MEASURES_FILES_STORAGE_MODE=${MEASURES_FILES_STORAGE_MODE:-gs}
